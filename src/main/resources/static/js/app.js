f4529a7aecb98163a8019eb2ebaa5dbeb79d4a4f
Jakub Romaniszyn <khobar@interia.pl>
2017-02-27 20:21:47 +0100

    GIFTS-1: Add default navigation layout. To be changed when app will evolve

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -11,7 +11,7 @@ app.config(function ($routeProvider, $httpProvider, $locationProvider) {
         })
         .when('/list', {
             templateUrl: 'list.html',
-            controller: 'home'
+            controller: 'list'
         })
         .otherwise('/');
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
