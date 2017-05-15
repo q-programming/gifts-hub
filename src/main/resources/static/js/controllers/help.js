7ff23f85d9c2b8a203b161e3cde1925f9fb9dcb2
Jakub Romaniszyn <jakub.romaniszyn@gmail.com>
2017-05-15 17:05:35 +0200

    GIFTS-59: Add english basic help site

--- src/main/resources/static/js/controllers/help.js
+++ src/main/resources/static/js/controllers/help.js
@@ -1,9 +1,12 @@
-app.controller('help', ['$rootScope', '$scope', '$http', '$log', 'AlertService',
-    function ($rootScope, $scope, $http, $log, AlertService) {
+app.controller('help', ['$rootScope', '$scope', '$http', '$log', '$translate', 'AlertService',
+    function ($rootScope, $scope, $http, $log, $translate, AlertService) {
+        $translate("gift.status.claimed").then(function (translation) {
+            $scope.gift_tooltip = translation + " " + $rootScope.principal.fullname;
+        });
         $scope.tourReset = function () {
             var url = 'api/user/tour-reset';
             $http.post(url).then(
-                function (response) {
+                function () {
                     $log.debug("[DEBUG] Tour marked as incomplete");
                     $rootScope.principal.tourComplete = false;
                 }).catch(function (response) {
